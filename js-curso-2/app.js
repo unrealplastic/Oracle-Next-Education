f4cb996f060ac738c3e912783ac17739ea5cdd7b
Nicole Lisboa <nickliaboa@gmail.com>
2025-07-29 14:51:00 -0300

    criação da função que altera campos de texto

--- js-curso-2/app.js
+++ js-curso-2/app.js
@@ -1,5 +1,17 @@
-let titulo = document.querySelector('h1'); //seleciona o elemento 
+/*let titulo = document.querySelector('h1'); //seleciona o elemento 
 titulo.innerHTML = 'Jogo do número secreto'; //altera o conteúdo da tag
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';*/
+
+function exibirTextoTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto
+}
+
+exibirTextoTela('h1', 'Jogo do número secreto');
+exibirTextoTela('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute() {
+    console.log('O botão foi clicado');
+}
