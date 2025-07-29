ecc9767ada59bd722a42c74bcf795e7ffa757417
Nicole Lisboa <nickliaboa@gmail.com>
2025-07-29 14:36:40 -0300

    criação das variáveis e uso de query selector

---
+++ js-curso-2/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1'); //seleciona o elemento 
+titulo.innerHTML = 'Jogo do número secreto'; //altera o conteúdo da tag
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
